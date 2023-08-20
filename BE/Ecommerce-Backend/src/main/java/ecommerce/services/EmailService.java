beb44bb2552235d2bba6c6a6f136291b5abe69d9
MohitBDev <mohitbijwar@gmail.com>
2023-08-20 23:33:10 +0530

    update3

---
+++ BE/Ecommerce-Backend/src/main/java/ecommerce/services/EmailService.java
@@ -0,0 +1,30 @@
+package ecommerce.services;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        MimeMessage message=emailSender.createMimeMessage();
+        try {
+	        message.setSubject(subject);
+	        MimeMessageHelper helper=new MimeMessageHelper(message,true);
+	        helper.setFrom("noreply@ecommerce.com");
+	        helper.setTo(to); 
+	        helper.setText(text,true);
+	        emailSender.send(message);
+	        System.out.println("Mail Send");
+        }catch(Exception ex) {
+        	System.out.println("Error "+ex.getMessage());
+        }
+    }
+}
